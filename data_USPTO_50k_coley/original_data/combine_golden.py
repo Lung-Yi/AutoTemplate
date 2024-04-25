1f67c4e48313eddc408f2f1e5f1cd8098862837f
Lung-Yi <sun73912@gmail.com>
2024-04-25 21:56:55 +0800

    modify2

---
+++ data_USPTO_50k_coley/original_data/combine_golden.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+input_file = "data_processed.csv"
+input_df = pd.read_csv(input_file)
+output_lines = []
+
+for i in range(len(input_df)):
+    rxn_smiles = input_df.iloc[i]["rxn_smiles"]
+    reaction_id = input_df.iloc[i]["id"]
+    line = rxn_smiles + "\t" + reaction_id + "_" + str(i) + "\n"
+    output_lines.append(line)
+
+with open("USPTO_50k_coley.txt", "w") as g:
+    g.writelines(output_lines)
